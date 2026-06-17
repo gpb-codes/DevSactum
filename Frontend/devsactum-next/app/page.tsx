d8e7515309c59665b248290e2f1456120fbb2c76
PABLOESTRADA20 <pablo.maximiliano.cocio.estrada@gmail.com>
2026-06-17 13:04:06 -0400

    feat/anterior codigo

--- Frontend/devsactum-next/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShell } from "@/src/components/layout/AppShell"
-
-export default function RootPage() {
-  return <AppShell />
-}
--- Frontend/devsactum-next/components/notifications/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as NotificationsPage } from "./NotificationsPage"
-export { NotificationRow } from "./NotificationRow"
-export { NotificationIcon } from "./NotificationIcon"
